1c91df7a4956ea0dd4bc7d1ef36e6c64a8ce28a1
Jonathan Trujillo <trujillo.jonathan@gmail.com>
2012-07-18 12:24:17 -0430

    some cleanups

--- Test/OurAirportsData.cs
+++ Test/OurAirportsData.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Configuration;
 using OurAirportsData;
 using Xunit;
 
@@ -10,24 +9,24 @@ namespace Test
         [Fact]
         public void CountriesNotEmpty()
         {
-            string filePath = ConfigurationManager.AppSettings["OurAirports_CountriesPath"];
-            IEnumerable<Country> countries = Data.GetCountries(filePath);
+            var filePath = ConfigurationManager.AppSettings["OurAirports_CountriesPath"];
+            var countries = Data.GetCountries(filePath);
             Assert.NotEmpty(countries);
         }
 
         [Fact]
         public void RegionsNotEmpty()
         {
-            string filePath = ConfigurationManager.AppSettings["OurAirports_RegionsPath"];
-            IEnumerable<Region> regions = Data.GetRegions(filePath);
+            var filePath = ConfigurationManager.AppSettings["OurAirports_RegionsPath"];
+            var regions = Data.GetRegions(filePath);
             Assert.NotEmpty(regions);
         }
 
         [Fact]
         public void AirportsNotEmpty()
         {
-            string filePath = ConfigurationManager.AppSettings["OurAirports_AirportsPath"];
-            IEnumerable<Airport> airports = Data.GetAirports(filePath);
+            var filePath = ConfigurationManager.AppSettings["OurAirports_AirportsPath"];
+            var airports = Data.GetAirports(filePath);
             Assert.NotEmpty(airports);
         }
     }
